5198204617a5cb23070c5cbfbed5ad120ab68860
Richard Gan <richard@designedbymajor.ca>
2018-05-09 13:39:22 -0400

    SLOG-2 more logging and moved token check

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ const slackToken = config.slack.verificationToken;
 app.use(bodyParser.json());
 
 app.post('/log', (req, res) => {
-  if (req.body.type === 'url_verification') {
-    console.log('Received challenge from Slack, checking token...');
-    
-    if (req.body.token !== slackToken) {
-      console.error('Token received from Slack and token given do not match!');
-    }
+  console.log(`Received call of type ${req.body.type} || Token Status: ${req.body.token == slackToken}`);
 
-    else {
-      console.log('Token is OK, responding to challenge.');
-      res.json({ challenge: req.body.challenge });
-    }
+  if (req.body.token !== slackToken) {
+    console.error('Token received does not match the token provided by Slack. Ignoring call...');
+    return;
+  }
+
+  if (req.body.type === 'url_verification') {
+    console.log("Responding to Slack's Events URL verification request.");
+    res.json({ challenge: req.body.challenge });
   }
 });
 
