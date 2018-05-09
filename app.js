90b0ad046203d69ea1b72dd3de5faddbeabab010
Richard Gan <richard@designedbymajor.ca>
2018-05-09 09:18:37 -0400

    SLOG-1 Added response to challenge

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config.json');
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
+const slackToken = process.env.TOKEN || config.token;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+
+app.post('/log', (req, res) => {
+  if (req.body.type === 'url_verification') {
+    console.log('Received challenge from Slack, checking token...');
+    
+    if (req.body.token !== slackToken) {
+      console.error('Token received from Slack and token given do not match!');
+      throw Error('Mismatched tokens, please check the token.');
+    }
+
+    else {
+      console.log('Token is OK, responding to challenge.');
+      res.json({ challenge: req.body.challenge });
+    }
+  }
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
